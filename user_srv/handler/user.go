3b14fca43891f0b596bd55578bc7246553dcc144
EthanWalker <NorwichSnower@proton.me>
2025-02-01 01:09:02 +0800

    完成 handler 6个接口

--- user_srv/handler/user.go
+++ user_srv/handler/user.go
@@ -60,7 +60,6 @@ func Paginate(page, pageSize int) func(db *gorm.DB) *gorm.DB {
 
 // 分页查询
 func (s *UserServer) GetUserList(ctx context.Context, req *proto.PageInfo) (*proto.UserListResponse, error){
-	//获取用户列表
 	var users []model.User
 	result := global.DB.Find(&users)
 	if result.Error != nil {
@@ -80,7 +79,6 @@ func (s *UserServer) GetUserList(ctx context.Context, req *proto.PageInfo) (*pro
 }
 
 func (s *UserServer) GetUserByMobile(ctx context.Context, req *proto.MobileRequest) (*proto.UserInfoResponse, error){
-	//通过手机号码查询用户
 	var user model.User
 	result := global.DB.Where(&model.User{Mobile:req.Mobile}).First(&user)
 	if result.RowsAffected == 0 {
@@ -95,7 +93,6 @@ func (s *UserServer) GetUserByMobile(ctx context.Context, req *proto.MobileReque
 }
 
 func (s *UserServer) GetUserById(ctx context.Context, req *proto.IdRequest) (*proto.UserInfoResponse, error){
-	//通过id查询用户
 	var user model.User
 	result := global.DB.First(&user, req.Id)
 	if result.RowsAffected == 0 {
@@ -110,7 +107,6 @@ func (s *UserServer) GetUserById(ctx context.Context, req *proto.IdRequest) (*pr
 }
 
 func (s *UserServer) CreateUser(ctx context.Context, req *proto.CreateUserInfo) (*proto.UserInfoResponse, error){
-	//新建用户
 	var user model.User
 	result := global.DB.Where(&model.User{Mobile:req.Mobile}).First(&user)
 	if result.RowsAffected == 1 {
@@ -120,14 +116,21 @@ func (s *UserServer) CreateUser(ctx context.Context, req *proto.CreateUserInfo) 
 	user.Mobile = req.Mobile
 	user.NickName = req.NickName
 
-	//密码加密
-	options := &password.Options{16, 100, 32, sha512.New}
+	// 密码加密
+	options := &password.Options{
+		SaltLen:    16,
+		Iterations: 100,     // 迭代次数为 100
+		KeyLen:     32,      // 密钥的长度为 32
+		HashFunction: sha512.New,
+	}	
 	salt, encodedPwd := password.Encode(req.PassWord, options)
 	user.Password = fmt.Sprintf("$pbkdf2-sha512$%s$%s", salt, encodedPwd)
 
 	result = global.DB.Create(&user)
 	if result.Error != nil {
-		return nil, status.Errorf(codes.Internal, result.Error.Error())
+		// status.Errorf 函数的第二个参数必须是常量字符串, 格式化字符串不能是一个运行时计算或动态生成的值
+		// return nil, status.Errorf(codes.Internal, result.Error.Error()) // 错误
+		return nil, status.Errorf(codes.Internal, "内部错误: %v", result.Error.Error())
 	}
 
 	userInfoRsp := ModelToRsponse(user)
@@ -135,11 +138,10 @@ func (s *UserServer) CreateUser(ctx context.Context, req *proto.CreateUserInfo) 
 }
 
 func (s *UserServer) UpdateUser(ctx context.Context, req *proto.UpdateUserInfo) (*empty.Empty, error){
-	//个人中心更新用户
 	var user model.User
 	result := global.DB.First(&user, req.Id)
 	if result.RowsAffected == 0 {
-		return nil, status.Errorf(codes.NotFound, "用户不存在")
+		return nil, status.Errorf(codes.Internal, "内部错误: %v", result.Error.Error())
 	}
 
 	birthDay := time.Unix(int64(req.BirthDay), 0)
@@ -149,14 +151,19 @@ func (s *UserServer) UpdateUser(ctx context.Context, req *proto.UpdateUserInfo) 
 
 	result = global.DB.Save(&user)
 	if result.Error != nil {
-		return nil, status.Errorf(codes.Internal, result.Error.Error())
+		return nil, status.Errorf(codes.Internal, "内部错误: %v", result.Error.Error())
 	}
 	return &empty.Empty{}, nil
 }
 
+// 校验密码
 func (s *UserServer) CheckPassWord(ctx context.Context, req *proto.PasswordCheckInfo) (*proto.CheckResponse, error){
-	//校验密码
-	options := &password.Options{16, 100, 32, sha512.New}
+	options := &password.Options{
+		SaltLen:    16,
+		Iterations: 100,
+		KeyLen:     32,
+		HashFunction: sha512.New,
+	}
 	passwordInfo := strings.Split(req.EncryptedPassword, "$")
 	check := password.Verify(req.Password, passwordInfo[2], passwordInfo[3], options)
 	return &proto.CheckResponse{Success: check}, nil
